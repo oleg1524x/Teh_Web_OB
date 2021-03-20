59efb9bcc944dc220d11fc65cb3bdf772f17747d
oleg1524x <justoleg15@gmail.com>
2021-03-20 15:11:19 +0200

    layout

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import img1 from '../images/image.jpg';
+import './Home.css';
+function Home() {
+  return (
+    <div  className="container">
+    <div className='home'>
+      <h1>Home</h1>
+      
+      
+
+    </div>
+    </div>
+  );
+}
+
+export default Home;
